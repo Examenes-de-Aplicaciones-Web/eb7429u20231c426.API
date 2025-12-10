5b1048165255f115ccd498a99aefa47a67b9f8c4
Mr-Code-start <vitalybaca92@gmail.com>
2025-12-10 11:41:52 -0500

    feat: add Handle method for PickUpOrdersCommand in IOrdersCommandService

--- eb7429u20231c426.API/Operations/Domain/Services/IOrdersCommandService.cs
+++ eb7429u20231c426.API/Operations/Domain/Services/IOrdersCommandService.cs
@@ -6,4 +6,6 @@ namespace eb7429u20231c426.API.Operations.Domain.Services;
 public interface IOrdersCommandService
 {
     Task<Orders?> Handle(CreateOrdersCommand command);
+    
+    Task<Orders?> Handle(PickUpOrdersCommand command);
 }
