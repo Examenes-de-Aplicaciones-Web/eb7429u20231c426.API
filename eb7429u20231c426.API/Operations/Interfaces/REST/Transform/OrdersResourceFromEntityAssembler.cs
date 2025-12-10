9474b0d382ba97147e6c193602370a8d0043851a
Mr-Code-start <vitalybaca92@gmail.com>
2025-12-09 21:19:36 -0500

    feat: convert OrdersResourceFromEntityAssembler to static and include additional date properties

--- eb7429u20231c426.API/Operations/Interfaces/REST/Transform/OrdersResourceFromEntityAssembler.cs
+++ eb7429u20231c426.API/Operations/Interfaces/REST/Transform/OrdersResourceFromEntityAssembler.cs
@@ -3,13 +3,18 @@ using eb7429u20231c426.API.Operations.Interfaces.REST.Resources;
 
 namespace eb7429u20231c426.API.Operations.Interfaces.REST.Transform;
 
-public class OrdersResourceFromEntityAssembler
+public static class OrdersResourceFromEntityAssembler
 {
     public static OrdersResource ToResourceFromEntity(Orders entity)
     {
+        
         return new OrdersResource(
             entity.Id,
             entity.LockerId,
+            entity.PlacedAt,
+            entity.PickedUpAt, 
+            entity.CreatedDate,
+            entity.UpdatedDate,
             UserResourceFromEntityAssembler.ToResourceFromEntity(entity.User)
         );
     }
